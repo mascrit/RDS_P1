dd47e7ab9304340ba676ccd9c9aa5fd0da3f5365
Valdr Stiglitz <dark_reggae_93@comunidad.unam.mx>
2020-05-11 23:22:31 -0500

    Añadido manual windows

--- P3/configs/add_user.sh
+++ P3/configs/add_user.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 usuario=$1
 adduser $usuario
-#echo "Ingresa la contraseña linux del usuario"
-#passwd $usuario
 uid=$(id -u $usuario)
 echo "Ingresa la contraseña SAMBA del usuario"
 samba-tool user create $usuario --uid-number $uid
